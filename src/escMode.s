6c2ab6ecfd3d0f2f28848999910eeeb07ef52b2e
deraken02 <louisdelacroix3@gmail.com>
2022-05-24 20:33:54 +0200

    [Clean up] Simplify display of help

--- src/escMode.s
+++ src/escMode.s
@@ -2,32 +2,22 @@
 c:
     .byte ''
 help:
-    .string ".cache/help"
+    .string ".cache/help.txt"
 .text
 .global displayHelp
 .type displayHelp, @function
 displayHelp:
     push %rbp   /*Sauvegarde le pointeur de base*/
     movq %rsp, %rbp
-    pushq %rdi
+    pushq %rdi       /* file descriptor*/
     call clearTerm
-    movq $57, %rax  /* syscall fork */
-    syscall
-    cmp $0, %rax
-    jne parent
-    movq $59, %rax
-    movq $help, %rdi
-    movq $0, %rsi
-    movq $0, %rdx
-    syscall
-    jmp .end_display_help
-parent:
-    movq $61, %rax  /* sys_wait4*/
-    movq $(-1), %rdi
-    movq $0, %rsi
-    movq $0, %rdx
-    movq $0, %r10
-    syscall
+    movl $0, %esi
+    movl $help, %edi
+    movl $0, %eax
+    call open
+    movq %rax, %rdi
+    call displayContent
+    call closeFile
     call getchar
     call clearTerm
     popq %rdi       /* file descriptor*/
