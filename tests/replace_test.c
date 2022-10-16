6c1c0a7305a06463ab8c4196a4641d93b00b710f
deraken02 <louisdelacroix3@gmail.com>
2022-10-16 18:30:05 +0200

    [Erase] Add tests for erase

--- tests/replace_test.c
+++ tests/replace_test.c
@@ -2,7 +2,6 @@
 
 int main()
 {
-    printf("%s%c%s","Hello woilt",27,"[Dd");        /* Left direction key*/
-    printf("%c%c%c%c%c%c",127,127,127,'r',27,'q');  /* backspace */
+    printf("%s%c%s","Hello worlt",27,"[Dd");        /* Left direction key*/
     return 0;
 }
